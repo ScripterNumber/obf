f2081d6dd260e69166e4d69484558fbd8b4def56
ScripterNumber <infinixzedxxx@gmail.com>
2026-03-11 21:13:08 +0300

    Update obfuscate.js

--- api/obfuscate.js
+++ api/obfuscate.js
@@ -1,1 +1,43 @@
+const { v4: uuidv4 } = require('uuid');
+const { tokenize } = require('../lib/lexer');
+const { Parser } = require('../lib/parser');
+const { Compiler } = require('../lib/compiler');
+const { generateVM } = require('../lib/vm-template');
+const { save } = require('../lib/storage');
 
+module.exports = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const { code } = req.body;
+    if (!code || typeof code !== 'string') {
+      return res.status(400).json({ error: 'No code provided' });
+    }
+
+    const tokens = tokenize(code);
+    const parser = new Parser(tokens);
+    const ast = parser.parse();
+    const compiler = new Compiler();
+    const bytecode = compiler.compile(ast);
+    const vmScript = generateVM(bytecode);
+
+    const id = uuidv4();
+    save(id, vmScript);
+
+    const baseUrl = `https://${req.headers.host}`;
+
+    res.status(200).json({
+      id,
+      url: `${baseUrl}/api/fetch?id=${id}`,
+      script: vmScript
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
