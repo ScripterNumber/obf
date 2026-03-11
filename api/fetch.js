6f681161be0ce2d175ab6e8e225d5f8ee41ba695
ScripterNumber <infinixzedxxx@gmail.com>
2026-03-11 21:13:19 +0300

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -1,1 +1,20 @@
+const { load } = require('../lib/storage');
 
+module.exports = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).send('-- No ID provided');
+  }
+
+  const script = load(id);
+  if (!script) {
+    return res.status(404).send('-- Script not found or expired');
+  }
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send(script);
+};
